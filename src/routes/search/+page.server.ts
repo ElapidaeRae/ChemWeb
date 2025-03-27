cb892cc3e07eb8f77a63449500b8436b44b73786
ElapidaeRae <reubennoble16@gmail.com>
2025-03-27 01:19:28 +0000

    Refactor load function

--- src/routes/search/+page.server.ts
+++ src/routes/search/+page.server.ts
@@ -1,6 +1,5 @@
 import { page } from '$app/stores';
 
-let vear = $page.url.searchParams.get('query');
 export async function load() {
 	let pageName = $page.url.searchParams.get('query')
 	page.set(pageName);
